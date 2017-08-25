79f75eec1291abe83a7c7153368422804006382b
Yang Lu <me@yang.lu>
2017-08-25 10:58:54 -0700

    fixed python asyc syntax

--- helpers/pythonprac.py
+++ helpers/pythonprac.py
@@ -20,14 +20,13 @@ collection = db.trainings
 # results = collection.find()
 # print(results) 
 # print(collection.distinct('Text'))
-patrick_repub = []
-jasper_repub = []
+patrick = []
+jasper = []
 
 # You need to get the ids as well as the text so you can update afterwards.
 # You can use collection.find({}, {text: 1})
 # It gives all the ids and text.
-@asyncio.coroutine
-def patrick_populater(bin)
+async def patrick_populater():
     for tweet in collection.distinct('Text', {'Classification': 1}):
         meaningful_words = []
         nonum = re.sub("[\d*]", 
@@ -43,30 +42,35 @@ def patrick_populater(bin)
         clean = []
         for word in meaningful_words:
             clean.append(SnowballStemmer("english").stem(word))
-        patrick_repub.append(clean)
-        jasper_repub.append(1)
+        patrick.append(clean)
+        jasper.append(1)
+
+    for tweet in collection.distinct('Text', {'Classification': 0}):
+        meaningful_words = []
+        nonum = re.sub("[\d*]", 
+            "number ",
+            tweet)
+        letters_only = re.sub("[^a-zA-Z]",
+            " ",
+            nonum)
+        nourlwords = re.sub(r'^https?:\/\/.*[\r\n]*', 'http ', letters_only, flags=re.MULTILINE)
+        words =  nourlwords.lower().split()
+        stops = set(stopwords.words("english"))
+        meaningful_words = [w for w in words if not w in stops]
+        clean = []
+        for word in meaningful_words:
+            clean.append(SnowballStemmer("english").stem(word))
+        patrick.append(clean)
+        jasper.append(0)
+
+    return jasper, patrick;
+
+
+async def test():
+    jasper, patrick = await patrick_populater()
+    print(jasper, patrick)
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(loop.wait(print(patrick_repub)))
+loop.run_until_complete(test())
+loop.close()
 
-
-patrick_demo = []
-jasper_demo = []
-
-for tweet in collection.distinct('Text', {'Classification': 0}):
-    meaningful_words = []
-    nonum = re.sub("[\d*]", 
-        "number ",
-        tweet)
-    letters_only = re.sub("[^a-zA-Z]",
-        " ",
-        nonum)
-    nourlwords = re.sub(r'^https?:\/\/.*[\r\n]*', 'http ', letters_only, flags=re.MULTILINE)
-    words =  nourlwords.lower().split()
-    stops = set(stopwords.words("english"))
-    meaningful_words = [w for w in words if not w in stops]
-    clean = []
-    for word in meaningful_words:
-        clean.append(SnowballStemmer("english").stem(word))
-    patrick_demo.append(clean)
-    jasper_demo.append(0)
